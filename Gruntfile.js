e418675a45cd5aa0eb47e05de4da9f5b2b770634
Tony Atkins <tony@raisingthefloor.org>
2016-08-02 17:25:02 +0200

    GPII-1893: Added additional Fluid dependencies (in the right order) so that the sandbox can be used with the Fluid demo code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,20 @@
+/* eslint-env node */
 "use strict";
 
 module.exports = function (grunt) {
-
-    // TODO:  Clean up the project structure into cleanly separated pieces with their own standard src and tests.
-    // TODO:  Set up separate checks for the couchapp content, with a different .jshintrc
     grunt.initConfig({
-        jshint: {
-            src: ["./**/*.js", "email/**/*.js", "front-end/**/*.js", "import/**/*.js", "test/**/*.js"],
-            buildScripts: ["Gruntfile.js"],
-            options: {
-                jshintrc: true
-            }
+        eslint: {
+            src: ["./src/**/*.js", "./tests/**/*.js", "./*.js"]
         },
         jsonlint: {
-            src: ["config/**/*.json", "test/**/*.json", "unifier/**/*.json", "email/**/*.json", "front-end/**/*.json", "import/**/*.json", "test/**/*.json"]
+            src: ["src/**/*.json", "tests/**/*.json", "./*.json"]
         }
     });
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-eslint");
     grunt.loadNpmTasks("grunt-jsonlint");
-    grunt.loadNpmTasks("grunt-shell");
-    grunt.loadNpmTasks("grunt-gpii");
+
+    grunt.registerTask("lint", "Apply jshint and jsonlint", ["eslint", "jsonlint"]);
+
 
 };
