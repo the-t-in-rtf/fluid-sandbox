b41a2c42980174dc3da0ac959cb5a7310734fbe0
Tony Atkins <tony@raisingthefloor.org>
2018-03-02 11:13:23 +0100

    Updated dependencies and fixed loader to work with modern npm versions.

--- src/js/client/sandbox.js
+++ src/js/client/sandbox.js
@@ -59,7 +59,7 @@
     fluid.defaults("fluid.sandbox", {
         gradeNames: ["fluid.viewComponent"],
         componentPath: "fluid_sandbox_dynamic_component",
-        selectors: {
+            selectors: {
             options:      ".sandbox-options",
             markupInput:  ".sandbox-html-input",
             markupOutput: ".sandbox-html-output",
